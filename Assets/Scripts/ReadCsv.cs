4c1d96ff7a276369a381660c7987038fee2a6a0f
Connor Fullarton <connorgamedev@hotmail.com>
2021-07-17 15:42:43 +0100

    Finished labelling every piece and finished Part 2 though unable to get Nut to appear in text fields, also tidied up ReadCsv code.

--- Assets/Scripts/ReadCsv.cs
+++ Assets/Scripts/ReadCsv.cs
@@ -6,10 +6,10 @@ using System.IO;
 
 public class ReadCsv : MonoBehaviour
 {
-    // Hold a reference to the CSV
+    // Holds a reference to the CSV
     [SerializeField]
     private TextAsset _partsCSV;
-    // Hold an array of every object tag so that we can search for each one later
+    // Creates an array holding every object tag so that we can search for each one later
     private string[] _tags = {"Nut", "Bolt", "Ring", "Lid", "Glove Finger", "Glove Finger (No Ring)", "Piston", "Connector", "Hub", "Cog", "Washer", "Plate"};
     // Defines the character we use to separate between the fields in the CSV
     private char _fieldSeperator = ',';
@@ -19,29 +19,24 @@ public class ReadCsv : MonoBehaviour
         // Split up each field in the CSV and assign it to the fields array, there's only 1 line so don't need to worry about splitting lines as well
         string[] fields = _partsCSV.text.Split(_fieldSeperator);
 
+        // Cycles through the array for _tags and finds every object matching a tag
         foreach (string tag in _tags)
         {
             GameObject[] tagGameObjects = GameObject.FindGameObjectsWithTag(tag);
 
-
+            // Iterates through each object found with the tag specified earlier and gets a reference to the TextMesh component in each one
             foreach (GameObject obj in tagGameObjects)
-            {
-                //Debug.Log(obj.tag);
+            {     
                 TextMesh textMesh = obj.GetComponent<TextMesh>();
 
+                // Checks to see if any of the words pulled from the CSV matches the Game Object tag and assigns the text field
+                // in the TextMesh component the word in the CSV if it matches
                 foreach (string field in fields)
                 {
-                    Debug.Log(field);
                     if (field == obj.tag)
                     {
-                        //Debug.Log("It's a match");
                         textMesh.text = field;
                     }
-
-                    // else 
-                    // {
-                    //     return;
-                    // }
 
                 } 
                 
