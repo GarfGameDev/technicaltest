ab0b8a0f9c655143ca528633a5bab4cc401866a9
Connor Fullarton <connorgamedev@hotmail.com>
2021-07-17 11:22:39 +0100

    Implemented first working version of lerping individual pieces and improved the ability to rotate the model

---
+++ Assets/Scripts/LerpInfo.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpInfo : MonoBehaviour
+{
+    // Specify the distance each piece will move so that we can see them all separated
+    public float distance;
+}
--- Assets/Scripts/ModelInput.cs
+++ Assets/Scripts/ModelInput.cs
@@ -10,6 +10,16 @@ public class ModelInput : MonoBehaviour
     [SerializeField]
     private Slider _scaleSlider;
     private float _scaleValue;
+
+    private bool _canTap = true;
+
+    [SerializeField]
+    private Camera _camera;
+
+    private int _interpolationFramesCount = 5;
+    private int _elapsedFrames = 0;
+
+
     
 
     // Update is called once per frame
@@ -17,18 +27,29 @@ public class ModelInput : MonoBehaviour
     {
         RotateModel();
         ScaleModel();
+
+        if (_canTap == true) 
+        {
+            LerpPieces();
+        }
     }
 
     // Created separate function for readability
     void RotateModel() 
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(1))
         {
+            // prevent user from tapping and expanding pieces while rotating the model
+            _canTap = false;
             // Storing a reference to both axis of the mouse's current position as well as the speed to rotate it by
             // Doing this for better readability
             float mouseX = Input.GetAxis("Mouse X") * _rotationSpeed * Time.deltaTime;
             float mouseY = Input.GetAxis("Mouse Y") * _rotationSpeed *Time.deltaTime;
-            transform.Rotate(mouseY, mouseX, 0, Space.World);
+            transform.Rotate(mouseY, -mouseX, 0, Space.World);
+        }
+        else 
+        {
+            _canTap = true;
         }
     }
 
@@ -38,5 +59,24 @@ public class ModelInput : MonoBehaviour
         _scaleValue = _scaleSlider.value;
         // I'm wanting the model to scale evenly so I've set the x,y,z values of this vector 3 to the same
         transform.localScale = new Vector3(_scaleValue, _scaleValue, _scaleValue);
+    }
+
+    private void LerpPieces()
+    {
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            float interpolationRatio = (float)_elapsedFrames / _interpolationFramesCount; 
+            RaycastHit hit;
+            // Cast a ray from the camera view to the position of the mouse
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Get a reference to the LerpInfo script in each piece
+                LerpInfo lerp = hit.transform.GetComponent<LerpInfo>();                    
+                hit.transform.localPosition = Vector3.Lerp(new Vector3(-lerp.distance, 0, 0), Vector3.forward, interpolationRatio);
+
+            }
+        }
     }
 }
